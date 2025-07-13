48ae13588b52a428043d5ddb9bc0d56b7e804b55
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 12:29:24 +0530

    Pattern program

---
+++ T1Q15.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q15
+    {
+        public static void Main(string[] args)
+        {
+            int rows = 4; // Number of rows in the triangle
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine(); // Move to next line
+            }
+        }
+    }
+}
