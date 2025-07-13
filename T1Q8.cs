d061552f5680ae34f6d326adf80876111b79c18b
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 10:14:41 +0530

    Q. 7 & 8

---
+++ T1Q8.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q8
+    {
+        public static void Main(string[] args)
+        {
+            string str1, str2;
+            Console.Write("INPUT : ");
+            str1 = Console.ReadLine();
+            str2 = str1.ToUpper();
+            Console.Write("OUTPUT : " +str2);
+        }
+    }
+}
