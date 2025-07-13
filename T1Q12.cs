abd6c379c6acc24520d349b71e299ddc26e9cc54
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 12:22:21 +0530

    Output same as a input

---
+++ T1Q12.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q12
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("INPUT : ");
+            string name = Console.ReadLine(); 
+
+            Console.WriteLine("OUTPUT: " + name);
+        }
+    }
+}
