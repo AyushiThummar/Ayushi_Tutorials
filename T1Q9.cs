acff25ad314bc467c792dbbeeb9a17a01f32cd14
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 10:23:39 +0530

    Program file of Toggle case.

---
+++ T1Q9.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q9
+    {
+        // Regular method to toggle case
+        public static string ToToggle(string input)
+        {
+            string result = "";
+            foreach (char ch in input)
+            {
+                if (char.IsUpper(ch))
+                    result += char.ToLower(ch);
+                else if (char.IsLower(ch))
+                    result += char.ToUpper(ch);
+                else
+                    result += ch;
+            }
+            return result;
+        }
+        public static void Main(string[] args)
+        {
+            string str1, str2;
+            Console.Write("INPUT : ");
+            str1 = Console.ReadLine();
+            str2 = ToToggle(str1); // calling the regular method
+            Console.Write("OUTPUT : " + str2);
+        }
+    }
+}
