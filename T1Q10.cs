a51c77de1f25b090c07c4be234941b5dc42bb069
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 12:11:50 +0530

    Mask mobile number.

---
+++ T1Q10.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q10
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("INPUT : ");
+            string mobile = Console.ReadLine();
+
+            if (mobile.Length >= 5)
+            {
+                string firstPart = mobile.Substring(0, mobile.Length - 5);
+                string maskedPart = new string('X', 5);
+                string result = firstPart + maskedPart;
+
+                Console.WriteLine("OUTPUT: " + result);
+            }
+            else
+            {
+                Console.WriteLine("Invalid mobile number. It must have at least 5 digits.");
+            }
+        }
+    }
+}
