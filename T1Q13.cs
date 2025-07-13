23df493a0e6899183ec6f447df4174a512f6178b
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 12:25:02 +0530

    Fibonacci series

---
+++ T1Q13.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q13
+    {
+        public static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3;
+            int terms = 11; // Number of terms to print
+
+            Console.Write("Fibonacci Series: ");
+            Console.Write(n1 + " " + n2 + " ");
+
+            for (int i = 3; i <= terms; i++)
+            {
+                n3 = n1 + n2;
+                Console.Write(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+        }
+    }
+}
