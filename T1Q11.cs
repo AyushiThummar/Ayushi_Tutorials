af942d2808f5ffe7d31869fd47f6ade0edef4306
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-13 12:16:07 +0530

    Gender based name Mr. or Ms.

---
+++ T1Q11.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q11
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("NAME   : ");
+            string name = Console.ReadLine();
+
+            Console.Write("GENDER (M/F): ");
+            char gender = Char.ToUpper(Console.ReadKey().KeyChar); // Read single character and convert to uppercase
+            Console.WriteLine(); // move to next line
+
+            if (gender == 'M')
+            {
+                Console.WriteLine("OUTPUT : Mr. " + name);
+            }
+            else if (gender == 'F')
+            {
+                Console.WriteLine("OUTPUT : Ms. " + name);
+            }
+            else
+            {
+                Console.WriteLine("Invalid gender entered. Please enter only 'M' or 'F'.");
+            }
+        }
+    }
+}
