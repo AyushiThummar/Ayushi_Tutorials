010ff2d7986b91bc54b6c6aea83dba5192b44abc
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-14 17:24:05 +0530

    Sum of series like 5+55+555+___

---
+++ T1Q21.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q21
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Input the number of terms : ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("Input number : ");
+            int digit = int.Parse(Console.ReadLine());
+
+            int term = 0;
+            int sum = 0;
+
+            Console.Write("Series : ");
+            for (int i = 1; i <= n; i++)
+            {
+                term = term * 10 + digit; // Build the term like 5, 55, 555, ...
+                Console.Write(term);
+                if (i != n) Console.Write(" + ");
+                sum += term;
+            }
+
+            Console.WriteLine("\nThe Sum is : " + sum);
+        }
+    }
+}
