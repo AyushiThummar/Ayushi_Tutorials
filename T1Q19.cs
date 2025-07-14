4193e56286d0b7d1ee3e0a8a4d9e8bb26420713c
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-14 17:18:44 +0530

    Pyramid pattern

---
+++ T1Q19.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q19
+    {
+        public static void Main(string[] args)
+        {
+            int rows = 4; 
+
+            for (int i = 1; i <= rows; i++)
+            {
+                // Print leading spaces
+                for (int space = 1; space <= rows - i; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print asterisks with space
+                for (int star = 1; star <= i; star++)
+                {
+                    Console.Write("* ");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
