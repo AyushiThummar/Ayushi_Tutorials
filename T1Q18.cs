df96c73081501124cc45741cf09ef8cb776ff8f2
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-14 17:15:44 +0530

    Pattern program 2 with numbers

---
+++ T1Q18.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q18
+    {
+        public static void Main(string[] args)
+        {
+            int rows = 4; 
+            int num = 1;  // Starting number
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
