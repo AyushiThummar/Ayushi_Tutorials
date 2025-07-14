9c5cad9655afa9222a1310146e33167f350a1e52
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-14 17:20:11 +0530

    Numbers Pyramid

---
+++ T1Q20.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q20
+    {
+        public static void Main(string[] args)
+        {
+            int rows = 4; // Number of pyramid levels
+            int num = 1;  // Starting number
+
+            for (int i = 1; i <= rows; i++)
+            {
+                // Print leading spaces
+                for (int space = 1; space <= rows - i; space++)
+                {
+                    Console.Write(" ");
+                }
+
+                // Print numbers with space
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(num + " ");
+                    num++;
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
