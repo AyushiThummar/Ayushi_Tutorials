8481fd11b4d335f5e37a9323b7aead3581fae95a
Ayushi💕 <ayushithummar14@gmail.com>
2025-07-14 17:12:46 +0530

    Pattern program with numbers

---
+++ T1Q17.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Ayushi_Tutorials
+{
+    internal class T1Q17
+    {
+        public static void Main(string[] args)
+        {
+            // This program prints a right-angled triangle pattern with numbers
+            int rows = 4; 
+
+            for (int i = 1; i <= rows; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(j + " ");
+                }
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
